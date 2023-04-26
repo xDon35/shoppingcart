08ec0777e91e3e2a5f5ce246e7fb34744109aab1
xDon35 <89415345+xDon35@users.noreply.github.com>
2023-04-26 10:48:19 -0400

    Add files via upload

---
+++ ecom_cookies/js/data.js
@@ -0,0 +1,43 @@
+var products=[
+    {
+        "title": "Men’s Antora Jacket",
+        "price": 110,
+        "imgs":[
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A7QEY_N11_hero?hei=650&wid=555&qlt=50&resMode=sharp2&op_sum=0.9,1.0,8,0",
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A7QEY_N11_back?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A7QEY_N11_modelhood2?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A7QEY_N11_model34?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0"
+        ],
+        "size":[
+            "S", "M","L","XL"
+        ]
+    },
+    {
+        "title": "Women’s Class V Pullover",
+        "price": 110,
+        "imgs":[
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A534P_HZO_hero?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A534P_HZO_back?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A534P_HZO_model34?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A534P_HZO_modelalt6?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0"
+        ],
+        "size":[
+            "S", "M","L","XL"
+        ]
+    },
+    {
+        "title": "Big Kids’ Denali Jacket",
+        "price": 130,
+        "imgs":[
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A82UE_LV1_hero?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A82UE_LV1_back?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+            "https://images.thenorthface.com/is/image/TheNorthFace/NF0A82UE_LV1_model34?wid=1300&hei=1510&fmt=jpeg&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0"
+        ],
+        "size":[
+            "XS","S", "M","L","XL"
+        ]
+    }
+
+
+
+]
